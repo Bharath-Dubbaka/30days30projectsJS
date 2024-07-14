48a331685be56a0527a437f3f40ebec9b005f0ca
Bharath Dubbaka <bharath.reactdev@gmail.com>
2024-07-14 16:32:05 +0530

    added new date format for new notes created

--- 16-NotesAppJS/app.js
+++ 16-NotesAppJS/app.js
@@ -1,18 +1,57 @@
 const addNote = document.querySelector(".addNote");
 const container = document.querySelector(".container");
-console.log(container);
-addNote.addEventListener("click", function name(params) {
-   console.log("clicked");
-   const items = document.querySelectorAll(".items");
 
+addNote.addEventListener("click", function name(params) {
    const item = document.createElement("div");
    item.className = "note items";
    // FOR EDIT ONCLICK UPON A DIV
    item.setAttribute("contenteditable", "true");
-
+   //    Adding date as a opening text in the note
+   item.innerHTML = formattedDateFn();
    container.appendChild(item);
-   console.log(items);
 });
 
+let formattedDateFn = function (params) {
+   const currentDate = new Date();
+   console.log(currentDate.getMonth(), "currentDate.getMonth()");
+   // Extract Diff date variables
+   //    const year = currentDate.getFullYear();
+   //    const month = String(currentDate.getMonth() + 1).padStart(2, "0");
+   const day = String(currentDate.getDate()).padStart(2, "0");
+   const hours = String(currentDate.getHours()).padStart(2, "0");
+   const minutes = String(currentDate.getMinutes()).padStart(2, "0");
+   //    const seconds = String(currentDate.getSeconds()).padStart(2, "0");
 
-
+   // To get the day Name of the week
+   const days = [
+      "Sunday",
+      "Monday",
+      "Tuesday",
+      "Wednesday",
+      "Thursday",
+      "Friday",
+      "Saturday",
+   ];
+   //    For Month Number to Name
+   const months = [
+      "Jan",
+      "Feb",
+      "Mar",
+      "Apr",
+      "May",
+      "June",
+      "July",
+      "Aug",
+      "Sep",
+      "Oct",
+      "Nov",
+      "Dec",
+   ];
+   const weekday = days[currentDate.getDay()];
+   //    Month Data from newDate() is of 0 based, EG: 0 - Jan
+   const monthName = months[currentDate.getMonth()];
+   // Format the date string
+   const formattedDate = `${weekday}, ${monthName}/${day}, ${hours}:${minutes} :::`;
+   //    console.log(formattedDate);
+   return formattedDate;
+};
