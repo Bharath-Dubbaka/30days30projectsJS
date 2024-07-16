43f1d69f154cf2a7a1f4cb38d5495358944587fd
Bharath Dubbaka <bharath.reactdev@gmail.com>
2024-07-16 18:23:18 +0530

    reolved the prob with spaces converting, so change innerHTML to innertext and now proj is able to take input and speak

--- 17-TextToSpeechJS/app.js
+++ 17-TextToSpeechJS/app.js
@@ -5,39 +5,50 @@ let synth = window.speechSynthesis;
 if (synth.onvoiceschanged !== undefined) {
    synth.onvoiceschanged = () => voiceRender();
 }
+// synth.speak("utterance2");
 // above due to async nature of API
 //GOOD INSIGHTS ON ISSUE - READ WHOLE PAGE https://github.com/electron/electron/issues/22844
 //https://developer.mozilla.org/en-US/docs/Web/API/synthSynthesis/getVoices
 
 // selectors
-const textInput = document.querySelector(".text");
+const textInput = document.querySelector("#text");
+const textInputClass = document.querySelector(".text");
 const btn = document.querySelector(".btn");
-const voiceSelector = document.querySelector("#voiceSelector");
+const voiceSelect = document.querySelector("#voiceSelect");
 let voices = [];
 // functions
-// const inputVal = function (params) {
-//    //    console.log(textInput.innerHTML, "textInput.innerHTML");
-//    //    let utterance = new synthSynthesisUtterance(textInput.innerHTML);
-//    //    console.log(utterance, "utterance");
-//    //    synthSynthesis.speak(utterance);
-//    voices.map((voice) => {
-//       console.log(voice);
-//    });
-// };
+const inputVal = function (event) {
+   console.log(textInputClass.innerText);
+   event.preventDefault();
+   const utterThis = new SpeechSynthesisUtterance(textInputClass.innerText);
+   const selectedOption =
+      voiceSelect.selectedOptions[0].getAttribute("data-name");
+   for (let i = 0; i < voices.length; i++) {
+      if (voices[i].name === selectedOption) {
+         utterThis.voice = voices[i];
+      }
+   }
+   //    utterThis.pitch = pitch.value;
+   //    utterThis.rate = rate.value;
+   console.log(utterThis);
+   window.speechSynthesis.speak(utterThis);
+
+   textInputClass.blur();
+};
 
 let voiceRender = function (params) {
    voices = synth.getVoices();
-   console.log(voices);
+   //    console.log(voices);
    for (let i = 0; i < voices.length; i++) {
       //   let voice = voices[i];
       //   let elem = `<option value=${voice}>${voice}</option>`;
-      //   voiceSelector.appendChild(elem);
+      //   voiceSelect.appendChild(elem);
 
       //   const option = document.createElement("option");
       //   option.textContent = `${voices[i].name} (${voices[i].lang})`;
       //   option.setAttribute("data-lang", voices[i].lang);
       //   option.setAttribute("data-name", voices[i].name);
-      //   voiceSelector.appendChild(option);
+      //   voiceSelect.appendChild(option);
 
       const option = document.createElement("option");
       option.textContent = `${voices[i].name} (${voices[i].lang})`;
@@ -48,14 +59,11 @@ let voiceRender = function (params) {
 
       option.setAttribute("data-lang", voices[i].lang);
       option.setAttribute("data-name", voices[i].name);
-      console.log(option);
-      voiceSelector.appendChild(option);
+      //   console.log(option);
+      voiceSelect.appendChild(option);
    }
 };
 // voiceRender();
 
-// function populateVoiceList() {
-//    synth.getVoices(); // now should have an array of all voices
-// }
 // EventListener
-// btn.addEventListener("click", inputVal)
+btn.addEventListener("click", inputVal);
