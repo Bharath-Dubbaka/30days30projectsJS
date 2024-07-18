0d6fc9351c2930c1447ef86162b668e8c0dabc5e
Bharath Dubbaka <bharath.reactdev@gmail.com>
2024-07-18 12:28:50 +0530

    tried some more styles and layout options

--- 18-EnglishDictionaryJS/app.js
+++ 18-EnglishDictionaryJS/app.js
@@ -5,6 +5,8 @@ const inputEng = document.querySelector(".inputEng");
 const convertBtn = document.querySelector(".convertBtn");
 const results = document.querySelector(".results");
 
+// let voice = document.querySelector("#voice_word").innerText
+
 let onClickConvert = (params) => {
    console.log(inputEng.value);
    console.log(`${urlBase}${inputEng.value}`, "`${urlBase}${inputEng.value}`");
@@ -19,30 +21,23 @@ let onClickConvert = (params) => {
 convertBtn.addEventListener("click", onClickConvert);
 
 let APIfn = function (data) {
-   //    fetch(`${urlBase}${inputEng.value}`)
-   //       .then((res) => {
-   //          res.json();
-   //       })
-   //       .then((data) => {
-   //          console.log(data);
-   //       })
-   //       .catch((err) => {
-   //          console.log(err);
-   //       });
-
    console.log(data[0].meanings[0].definitions[0], "data");
    let defs = data[0].meanings[0].definitions[0].definition;
+   let defs2 = data[0].meanings[0].definitions[1].definition;
    //    defs.forEach((defs) => {
    console.log(defs);
    //   const item = `<div className="items">{defs.definition}</div>`;
-   const item = document.createElement("div");
-   item.className = "item";
-   item.innerHTML = defs;
-   results.innerHTML = "";
-   results.appendChild(item);
-   //    });
+   //    const item = document.createElement("div");
+   //    item.className = "item";
+   //    item.innerHTML = defs;
+   //    results.innerHTML = "";
    //    results.appendChild(item);
-   //    data.forEach(element => {
-   //     elem
-   //    });
+   console.log(data[0], "hdhfh");
+   //    document.querySelector("#voice_word").innerText = data[0].word;
+   document.querySelector(
+      "#example"
+   ).innerHTML = `${data[0].word}, ${data[0].phonetic}`;
+   document.querySelector("#def").innerHTML = `DEF: ${defs}`;
+
+   document.querySelector("#meaning").innerHTML = `DEF 2 ${defs2}`;
 };
